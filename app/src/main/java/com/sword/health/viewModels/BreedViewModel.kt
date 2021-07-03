e3bbeb95eea2ef05c5c2902761a1f8b900a225ae
pedro <bertucci.dev@gmail.com>
2021-07-04 00:00:33 +0100

    was add viewModel for received data from repository.

---
+++ app/src/main/java/com/sword/health/viewModels/BreedViewModel.kt
@@ -0,0 +1,63 @@
+package com.sword.health.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sword.health.models.Breed
+import com.sword.health.models.Image
+import com.sword.health.remote.Status
+import com.sword.health.repositories.BreedRepository
+import com.sword.health.utils.Event
+import kotlinx.coroutines.*
+import javax.inject.Inject
+
+class BreedViewModel @Inject constructor(private val repository: BreedRepository) : ViewModel() {
+    private val breedsMutableData = MutableLiveData<ArrayList<Breed>>()
+    val breedsLiveData: LiveData<ArrayList<Breed>> get() = breedsMutableData
+
+    private val photoMutableData = MutableLiveData<ArrayList<Image>>()
+    val photoLiveData: LiveData<ArrayList<Image>> get() = photoMutableData
+
+    private val errorMutableData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String> get() = errorMutableData
+
+    fun getBreeds()  {
+        viewModelScope.launch {
+            val response = repository.getBreeds()
+
+            if(response.status == Status.SUCCESS) {
+                breedsMutableData.value = response.data
+
+            } else {
+                errorMutableData.value = response.message
+            }
+        }
+    }
+
+    fun getBreedsByName(name: String)  {
+        viewModelScope.launch {
+            val response = repository.getBreedsByName(name = name)
+
+            if(response.status == Status.SUCCESS) {
+                breedsMutableData.value = response.data
+
+            } else {
+                errorMutableData.value = response.message
+            }
+        }
+    }
+
+    fun getBreedPhoto(breedId: String)  {
+        viewModelScope.launch {
+            val response = repository.getBreedPhoto(breedId = breedId)
+
+            if(response.status == Status.SUCCESS) {
+                photoMutableData.value = response.data
+
+            } else {
+                errorMutableData.value = response.message
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/sword/health/viewModels/BreedViewModelTest.kt
@@ -0,0 +1,134 @@
+package com.sword.health.viewModels
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.sword.health.MainCoroutineRule
+import com.sword.health.repositories.FakeBreedRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class BreedViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: BreedViewModel
+
+    private val fakeBreedRepository = FakeBreedRepository()
+
+    @Before
+    fun setUp() {
+        viewModel = BreedViewModel(fakeBreedRepository)
+    }
+
+    @Test
+    fun `should fetch breeds and return a error`() {
+        fakeBreedRepository.shouldReturnNetworkError = true
+        viewModel.getBreeds()
+
+        val value = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(value.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch breeds and return a breed`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = false
+
+        viewModel.getBreeds()
+
+        val value = viewModel.breedsLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 1)
+    }
+
+    @Test
+    fun `should fetch breeds and return empty`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = true
+
+        viewModel.getBreeds()
+
+        val value = viewModel.breedsLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 0)
+    }
+
+    @Test
+    fun `should fetch breeds by name and return a breed`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = false
+
+        viewModel.getBreedsByName("test")
+
+        val value = viewModel.breedsLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 1)
+    }
+
+    @Test
+    fun `should fetch breeds by name and return a error`() {
+        fakeBreedRepository.shouldReturnNetworkError = true
+        viewModel.getBreeds()
+
+        val value = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(value.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch breeds by name repository and return empty`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = true
+
+        viewModel.getBreedsByName("test")
+
+        val value = viewModel.breedsLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 0)
+    }
+
+    @Test
+    fun `should fetch photo and return a error`() {
+        fakeBreedRepository.shouldReturnNetworkError = true
+        fakeBreedRepository.shouldReturnEmptyValues = false
+
+        viewModel.getBreedPhoto("12345")
+
+        val value = viewModel.errorLiveData.getOrAwaitValueTest()
+
+        assert(value.isNotEmpty())
+    }
+
+    @Test
+    fun `should fetch photo and return empty`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = true
+
+        viewModel.getBreedPhoto("12345")
+
+        val value = viewModel.photoLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 0)
+    }
+
+    @Test
+    fun `should fetch photo and return a photo`() {
+        fakeBreedRepository.shouldReturnNetworkError = false
+        fakeBreedRepository.shouldReturnEmptyValues = false
+
+        viewModel.getBreedPhoto("12345")
+
+        val value = viewModel.photoLiveData.getOrAwaitValueTest()
+
+        assert(value.size == 1)
+    }
+}
